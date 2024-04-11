cd25318a1303c672683654c42c6aa65b3a6a3e44
Carlo Ruaro <cruaro@protonmail.com>
2024-04-11 02:44:07 +0200

    Clickable dropdown menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-import './style.css';
+import "./style.css";
+
+const menu = document.getElementById("menu-title");
+const menuList = document.getElementById("menu-list");
+
+menu.addEventListener("click", () => {
+  menuList.classList.toggle("show-menu");
+  menuList.classList.toggle("hide");
+});
